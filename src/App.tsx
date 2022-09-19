a7096e2e4e02679b7d9773c5a7ee284d809a8c1a
JAENY <wjswodus22@naver.com>
2022-09-19 23:33:13 +0900

    master: Update router path Router Path Update: SignIn + SignUp => Sign

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from 'app.layout/Header';
 import Footer from 'app.layout/Footer';
 import Main from 'pages/main';
-import SignIn from 'pages/signin';
-import SignUp from 'pages/signup';
+import Sign from 'pages/sign';
 import ToDoPage from 'pages/todo';
 
 function App() {
@@ -14,8 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />}></Route>
         <Route path="/todo" element={<ToDoPage />}></Route>
-        <Route path="/signin" element={<SignIn />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/sign" element={<Sign />}></Route>
       </Routes>
       <Footer />
     </Router>
--- src/pages/signup.tsx
+++
@@ -1,5 +0,0 @@
-const SignUp = () => {
-  return <section>회원가입페이지</section>;
-};
-
-export default SignUp;
